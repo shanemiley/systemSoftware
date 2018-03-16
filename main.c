f7ed31f2d680f07b7ff29187ef5554121805984d
shanemiley <shanermiley@gmail.com>
2018-03-16 19:41:56 +0000

    added src dest

--- main.c
+++ main.c
@@ -20,15 +20,15 @@ int main()
     time_t now;
     struct tm newyear;
     double seconds;
-    char *src = "home/shane/Desktop/SSAssignment/testsrc.txt"; 
-    char *dest = "/home/shane/Desktop/SSAssignment/testdest.txt";
+    char *src = "/home/shane/Desktop/SSAssignment/var/www/html/live/"; 
+    char *dest = "/home/shane/Desktop/SSAssignment/var/www/html/intranet/";
     time(&now);  /* get current time; same as: now = time(NULL)  */
     newyear = *localtime(&now);
-    newyear.tm_hour = 15; 
-    newyear.tm_min = 40; 
+    newyear.tm_hour = 19; 
+    newyear.tm_min = 41; 
     newyear.tm_sec = 0;
   
-     //time(&now);
+     
    while(1) {
              sleep(1);
              
